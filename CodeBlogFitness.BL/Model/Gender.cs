ef2da1a0352c4f1aa1fad605deeb22db9acca732
semlebedev <semlebedev_i@mail.ru>
2021-03-15 20:51:07 +0500

    Создана модель пользователя.

---
+++ CodeBlogFitness.BL/Model/Gender.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogFitness.BL.Model
+{
+    public class Gender
+    {
+        public string Name { get; }
+
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Gender can't be null.", nameof(name));
+            }
+
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ CodeBlogFitness.BL/Model/User.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogFitness.BL.Model
+{
+    public class User
+    {
+        public string Name { get; }
+        public Gender Gender { get; }
+        public DateTime BirthDay { get; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        
+        public User(string name, Gender gender, DateTime birthDay, double weight, double height)
+        {
+            #region Check
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Name can't be null.", nameof(name));
+            }
+            if(gender == null)
+            {
+                throw new ArgumentNullException("Gender can't be null.", nameof(gender));
+            }
+            if(birthDay < DateTime.Parse("01.01.1900") || birthDay >= DateTime.Now)
+            {
+                throw new ArgumentException("Error date.", nameof(birthDay));
+            }
+            if(weight <= 10)
+            {
+                throw new ArgumentException("Error weight.", nameof(weight));
+            }
+            if(height <= 10)
+            {
+                throw new ArgumentException("Error height.", nameof(height));
+            }
+            #endregion
+
+            Name = name;
+            Gender = gender;
+            BirthDay = birthDay;
+            Weight = weight;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
